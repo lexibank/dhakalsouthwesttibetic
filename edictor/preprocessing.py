22f287ca70eb205acf355079b0585cc3d328ee16
lingulist <mattis.list@lingpy.org>
2022-04-14 22:40:29 +0200

    update new files

--- edictor/preprocessing.py
+++ edictor/preprocessing.py
@@ -1,18 +1,25 @@
 from lingpy import *
 from lingpy.sequence.sound_classes import syllabify
 from lingpy.compare.partial import Partial
+from lingrex.cognates import common_morpheme_cognates
 
 def run(wordlist):
     wordlist.add_entries("tokens", "tokens", lambda x: syllabify(x),
             override=True)
     part = Partial(wordlist)
-    part.partial_cluster(method="sca", threshold=0.45, ref="cogids")
-    part.add_cognate_ids("cogids", "cogid", idtype="strict")
+    part.get_partial_scorer(runs=10000)
+    part.partial_cluster(method="lexstat", threshold=0.45, ref="cogids")
+    common_morpheme_cognates(
+            part,
+            cognates="cogids",
+            ref="cogid",
+            morphemes="morphemes")
     
     alms = Alignments(part, ref="cogids")
     alms.align()
     D = {0: ["doculect", "concept", "value", "form", "tokens", "cogids",
+        "morphemes",
         "cogid", "alignment"]}
-    for idx in part:
-        D[idx] = [part[idx, h] for h in D[0]]
+    for idx in alms:
+        D[idx] = [alms[idx, h] for h in D[0]]
     return D
